8ff5c4309e2229f753b95517326b992a611dfb19
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2019-05-11 14:55:22 +0200

    introduce invoice positions

--- src/app/invoicing/invoice-positions/invoice-positions.component.ts
+++ src/app/invoicing/invoice-positions/invoice-positions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceItem } from '../model/item';
 
 @Component({
   selector: 'app-invoice-positions',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice-positions.component.scss']
 })
 export class InvoicePositionsComponent implements OnInit {
+  private positions: InvoiceItem[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.positions.push({});
+    this.positions.push({});
   }
 
+  addPosition(): void {
+    this.positions.push({});
+  }
+
+  removePosition(position: InvoiceItem): void {
+    this.positions = this.positions.filter(p => p !== position);
+  }
 }
