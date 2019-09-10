2822bc6fe0b4d972267df5c68f0e91dcafdabf9f
Filip <lipowiecki.f@gmail.com>
2019-09-10 21:12:40 +0200

    done

--- src/app/invoicing/invoice-positions/invoice-positions.component.ts
+++ src/app/invoicing/invoice-positions/invoice-positions.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { InvoiceItem, InvoiceItemFactory, InvoiceData } from "../model/item";
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-invoice-positions",
@@ -8,17 +8,14 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ["./invoice-positions.component.scss"]
 })
 export class InvoicePositionsComponent implements OnInit {
-
-
   public invoiceForm = new FormGroup({
-    firstName: new FormControl(''),
-    firstAddress: new FormControl(''),
+    firstName: new FormControl(""),
+    firstAddress: new FormControl(""),
     firstNIP: new FormControl(0),
-    secondName: new FormControl(''),
-    secondAddress: new FormControl(''),
+    secondName: new FormControl(""),
+    secondAddress: new FormControl(""),
     secondNIP: new FormControl(0)
   });
-
 
   @Input()
   private positions: InvoiceItem[] = new Array<InvoiceItem>();
@@ -33,15 +30,29 @@ export class InvoicePositionsComponent implements OnInit {
     this.invoiceItemFactory = new InvoiceItemFactory();
   }
 
-  ngOnInit() {for (let i = 0; i < localStorage.length; i++) {
-    const test = JSON.parse(
-      localStorage.getItem(localStorage.key(i))
-    ) as InvoiceItem;
-    if (test) {
-      this.positions.push(test);
+  ngOnInit() {
+    debugger;
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage['invoiceData'] === null) {
+        const test = JSON.parse(
+          localStorage.getItem(localStorage.key(i))
+        ) as InvoiceItem;
+        if (test) {
+          this.positions.push(test);
+        }
+      }
     }
-    console.log(this.positions);
-  }}
+
+    const testData = JSON.parse(
+      localStorage.getItem("invoiceData")
+    ) as InvoiceData;
+    this.invoiceForm.controls["firstName"].setValue(testData.firstName);
+    this.invoiceForm.controls["firstAddress"].setValue(testData.firstAddress);
+    this.invoiceForm.controls["firstNIP"].setValue(testData.firstNIP);
+    this.invoiceForm.controls["secondName"].setValue(testData.secondName);
+    this.invoiceForm.controls["secondAddress"].setValue(testData.secondAddress);
+    this.invoiceForm.controls["secondNIP"].setValue(testData.secondNIP);
+  }
 
   addPosition(): void {
     this.positions.push(this.invoiceItemFactory.newInvoiceItem());
@@ -58,12 +69,14 @@ export class InvoicePositionsComponent implements OnInit {
   }
 
   savePositions() {
-    this.invoiceData.firstName = this.invoiceForm['firstName'];
-    this.invoiceData.firstAddress = this.invoiceForm['firstAddress'];
-    this.invoiceData.firstNIP = this.invoiceForm['firstNIP'];
-    this.invoiceData.secondName = this.invoiceForm['secondName'];
-    this.invoiceData.secondAddress = this.invoiceForm['secondAddress'];
-    this.invoiceData.secondNIP = this.invoiceForm['secondNIP'];
+    console.log(this.invoiceForm);
+
+    this.invoiceData.firstName = this.invoiceForm.controls["firstName"].value;
+    this.invoiceData.firstAddress = this.invoiceForm.controls["firstAddress"].value;
+    this.invoiceData.firstNIP = this.invoiceForm.controls["firstNIP"].value;
+    this.invoiceData.secondName = this.invoiceForm.controls["secondName"].value;
+    this.invoiceData.secondAddress = this.invoiceForm.controls["secondAddress"].value;
+    this.invoiceData.secondNIP = this.invoiceForm.controls["secondNIP"].value;
     let iterator = 1;
     this.positions.forEach(invoiceItem => {
       localStorage.setItem(
@@ -72,10 +85,7 @@ export class InvoicePositionsComponent implements OnInit {
       );
       iterator++;
     });
-    localStorage.setItem(
-      "invoiceData",
-      JSON.stringify(this.invoiceData)
-    );
-    alert('Do you want to save your invoice?');
+    localStorage.setItem("invoiceData", JSON.stringify(this.invoiceData));
+    alert("Do you want to save your invoice?");
   }
 }
